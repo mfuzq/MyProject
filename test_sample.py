0c1f71ebe55d30d35776ed33df156d4278cfbc0a
mfuzq <eatlove89@gmail.com>
2024-05-30 16:21:07 +0300

    setup

---
+++ test_sample.py
@@ -0,0 +1,9 @@
+# test_sample.py
+import unittest
+
+class TestSample(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
